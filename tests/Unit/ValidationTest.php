9968131691fc03813c1d4c5a7843aacef82cdd1c
Dawid <david.koleczko.cobc@gmail.com>
2025-10-16 20:50:45 +0200

    add comment to unit tests

--- tests/Unit/ValidationTest.php
+++ tests/Unit/ValidationTest.php
@@ -11,6 +11,9 @@ class ValidationTest extends TestCase
 {
     use RefreshDatabase;
 
+    /* Unit tests are ideal for testing separate functions, in this task there was not enough logic to justify spreading the code into services
+      and having own functions.Therefore I have written an example Unit test but it would be much better to test a single function. */
+
     public function test_post_requires_title_and_content()
     {
         $user = User::factory()->create();
